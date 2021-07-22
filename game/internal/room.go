a2a1a8c9d3ed5340e2b8c7e179949c839a6951c6
rosco.l <rosco.l@LGroup.global>
2021-07-22 10:42:24 +0800

    捷豹奇趣->集團奇趣

--- game/internal/room.go
+++ game/internal/room.go
@@ -76,7 +76,7 @@ const (
 )
 
 const (
-	PTXFFC = "https://manycai.com/K2601968389c853/PTXFFC-1.json"
+	QIQFFC = "https://manycai.com/K2601968389c853/QIQFFC-1.json"
 	HNFFC  = "https://manycai.com/K2601968389c853/hn60-1.json"
 )
 
@@ -471,7 +471,7 @@ func (r *Room) CaiYunApi() bool {
 	if r.RoomId == "1" {
 		caiYuan = HNFFC
 	} else if r.RoomId == "2" {
-		caiYuan = PTXFFC
+		caiYuan = QIQFFC
 
 	}
 	keyReqPrize.Lock()
